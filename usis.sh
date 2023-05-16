e9c7381e583013254a2091ff66fd5b5f2ce9fe9b
George Eskandar <georgesbassem@yahoo.com>
2023-05-16 14:30:19 +0200

    info.json

---
+++ usis.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -l
+
+#Slurm parameters
+#SBATCH --job-name=test
+#SBATCH --output=usis%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=4-23:00:00
+#SBATCH --mem=64G
+#SBATCH --gpus=1
+#SBATCH --partition=highperf
+
+# Activate everything you need
+
+conda activate /usrhomes/m142/anaconda3/envs/myenv
+# Run your python code
+
+python train.py --name usis_wavelet --dataset_mode cityscapes --gpu_ids 0 \
+--dataroot /data/public/cityscapes --batch_size 1  \
+--netDu wavelet \
+--model_supervision 0 --netG wavelet --channels_G 16
+
+#python train.py --name usis --dataset_mode cityscapes --gpu_ids 0 \
+#--dataroot /data/public/cityscapes --batch_size 1  \
+#--netDu wavelet_decoder \
+#--model_supervision 0 --netG 0 --channels_G 16
+
+
+#python test.py --name oasis_cityscapes_wavelet_disc --dataset_mode cityscapes --gpu_ids 0 \
+#--dataroot /data/public/cityscapes --batch_size 1 \
+#--channels_G 64 --netG 0 \
+#--model_supervision 0 \
+#--ckpt_iter best
+
+
+
